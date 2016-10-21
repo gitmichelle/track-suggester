4bdc619db75352965122e32908d56062da18cb72
Michelle Llaguno <michellellaguno@gmail.com>
2016-10-21 11:12:53 -0700

    start js portion.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("form#tracksuggester").submit(function(event) {
+    var question1 = $("input:radio[name=question1]:checked").val();
+    var question2 = $("input:radio[name=question2]:checked").val();
+    var question3 = $("input:radio[name=question3]:checked").val();
+    var question4 = $("input:radio[name=question4]:checked").val();
+    var question5 = $("input:radio[name=question5]:checked").val();
+    var question6 = $("input:radio[name=question6]:checked").val();
+
+
+    event.preventDefault();
+  });
+});
