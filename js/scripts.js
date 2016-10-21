3310fc1427abcc8030d5c37b4a4052c005217e69
Michelle Llaguno <michellellaguno@gmail.com>
2016-10-21 11:46:36 -0700

    Add js functions, logic. Add results portion to HTML.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   $("form#tracksuggester").submit(function(event) {
-    var question1 = $("input:radio[name=question1]:checked").val();
-    var question2 = $("input:radio[name=question2]:checked").val();
-    var question3 = $("input:radio[name=question3]:checked").val();
-    var question4 = $("input:radio[name=question4]:checked").val();
-    var question5 = $("input:radio[name=question5]:checked").val();
-    var question6 = $("input:radio[name=question6]:checked").val();
+    var question1 = parseInt($("input:radio[name=question1]:checked").val());
+    var question2 = parseInt($("input:radio[name=question2]:checked").val());
+    var question3 = parseInt($("input:radio[name=question3]:checked").val());
+    var question4 = parseInt($("input:radio[name=question4]:checked").val());
+    var question5 = parseInt($("input:radio[name=question5]:checked").val());
+    var question6 = parseInt($("input:radio[name=question6]:checked").val());
+    var total = (question1 + question2 + question3 + question4 + question5 + question6);
 
-
+    if (total <= 9){
+      var total
+    }
     event.preventDefault();
   });
 });
